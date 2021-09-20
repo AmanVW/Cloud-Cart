e8611d051632736542fab35fb3828d07fd67413a
AmanVW <amanwalke97@gmail.com>
2021-09-20 20:13:55 +0530

    first commit

---
+++ member-bs/src/test/java/com/cloudmart/memberbs/MemberBsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudmart.memberbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberBsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
